80d58342fb6084693471cb533eaec2aa35c52554
dakshna <dakshnasuba@gmail.com>
2025-10-07 12:42:17 +0530

    4th from localgit

--- src/main/java/com/example/security/MySpringBootApplication.java
+++ src/main/java/com/example/security/MySpringBootApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootApplication {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("new jwt changes");		
+=======
+		System.out.println("new jwt changes");
+		System.out.println("change from localgit");
+>>>>>>> Stashed changes
 		SpringApplication.run(MySpringBootApplication.class, args);
 	}
 }
